d6950d6afea4669b7bb070eab5f54716bbc2eb7b
Hadi Mohagheghi <hadimohagheghi@live.com>
2024-06-24 16:23:22 +0330

    به دام انداختن خطایی که رخ داده لاگش کردیم خروجی رو به صورت استاندارد و یک دست برگردوندیم

---
+++ WebFramework/Middlewares/CustomExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WebFramework.Api;
+
+namespace WebFramework.Middlewares
+{
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next,ILogger<CustomExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e,"خطایی رخ داده است");
+                
+                var apiResult = new ApiResult(false,ApiResultStatusCode.ServerError);
+                var json = JsonConvert.SerializeObject(apiResult);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
